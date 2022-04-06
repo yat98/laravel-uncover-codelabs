eac631379023464464678c55eff77ec18786c9a4
yat98 <hidayatchandra08@gmail.com>
2022-04-07 00:32:36 +0800

    7.8 Bootstrap CSS framework

--- resources/views/university/student.blade.php
+++ resources/views/university/student.blade.php
@@ -5,26 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student List</title>
-    <link rel="stylesheet" href="/css/my-style.css">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Student List</h1>
-    <ol>
-        <?php
-            foreach ($students as $student) {
-                echo "<li>{$student}</li>";
-            }    
-        ?>
-    </ol>
-    <div>
-        <img src="/img/people1.jpg">
-        <img src="/img/people2.jpg">
-        <img src="/img/people3.jpg">
-        <img src="/img/people4.jpg">
+    <div class="container text-center mt-4">
+        <h1>Student List</h1>
+        <ol class="list-group my-4">
+            <?php
+                foreach ($students as $student) {
+                    echo "<li class=\"list-group-item\">{$student}</li>";
+                }    
+            ?>
+        </ol>
+        <div>
+            <img class="rounded-circle img-thumbnail m-2" src="/img/people1.jpg">
+            <img class="rounded-circle img-thumbnail m-2" src="/img/people2.jpg">
+            <img class="rounded-circle img-thumbnail m-2" src="/img/people3.jpg">
+            <img class="rounded-circle img-thumbnail m-2" src="/img/people4.jpg">
+        </div>
+        <div>
+            Copyright © <?= date('Y') ?> Duniailkom
+        </div>
     </div>
-    <div>
-        Copyright © <?= date('Y') ?> Duniailkom
-    </div>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
     <script src="/js/my-script.js"></script>
 </body>
 </html>
