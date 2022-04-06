4985416bbe934c7a4b3643f0509c4c52b83d3ae8
yat98 <hidayatchandra08@gmail.com>
2022-04-06 23:26:45 +0800

    7.3 Membuat view

---
+++ resources/views/student.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student List</title>
+</head>
+<body>
+    <h1>Student List</h1>
+    <ol>
+        <li>John Doe</li>
+        <li>Hidayat Chandra</li>
+        <li>Rudy</li>
+    </ol>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('student', function () {
+    return View::make('student');
 });
