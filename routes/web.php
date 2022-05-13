32feb40d862e0cc125b05602c0c65bf4ff4ac6e9
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-05-13 17:02:38 +0800

    11.5 Memindahkan file controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\Admin\PageController;
 
 /*
 |--------------------------------------------------------------------------
