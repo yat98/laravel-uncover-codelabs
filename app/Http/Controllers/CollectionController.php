ad17cb994c0ee76056142e9295aeed1dc2439aba
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-05-15 16:46:29 +0800

    12.4 Collection method

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -36,11 +36,51 @@ class CollectionController extends Controller
     }
 
     public function thirdCollection(){
+        $collections = new Collection($this->numbers);
 
+        dump($collections->sum());
+        dump($collections->avg());
+        dump($collections->max());
+        dump($collections->min());
+        dump($collections->median());
+        dump($collections->random());
+        echo $collections->concat([1,8,9]).'<br>';
+        dump($collections->contains(3));
+        dump($collections->contains(8));
+        echo $collections->unique().'<br>';
+        dump($collections->first());
+        dump($collections->last());
+        dump($collections->count());
+        echo $collections->sort();
     }
 
     public function fourthCollection(){
-
+        $collections = collect([
+            "nama" => "Laura",
+            "sekolah" => "SMA 5 Lampung",
+            "kota" => "Lampung",
+            "jurusan" => "IPA",
+        ]);
+        dump($collections->get('sekolah'));
+        dump($collections->get('umur', 17));
+        dump($collections->has('jurusan'));
+        dump($collections->has('umur'));
+        $results = $collections->replace([
+            "sekolah" => "SMA 5 Palembang",
+            "kota" => "Palembang",
+        ]);
+        dump($results);
+        dump($collections->forget('sekolah'));
+        dump($collections->flip());
+        dump($collections->keys());
+        dump($collections->values());
+        dump($collections->search('IPA'));
+        $collections->each(function ($val, $key){
+            echo "$key: $val <br>";
+        });
+        foreach($collections as $key => $val){
+            echo "$key: $val <br>";
+        }
     }
 
     public function fifthCollection(){
