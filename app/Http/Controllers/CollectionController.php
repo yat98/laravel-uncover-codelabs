606a1e0e517f61c41a27af48a0ed2414212bc48c
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-05-15 16:24:52 +0800

    12.3 Mengakses collection

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -7,14 +7,32 @@ use Illuminate\Support\Collection;
 
 class CollectionController extends Controller
 {
+    protected $numbers = [1, 9, 3, 4, 5, 3, 5, 7];
+
     public function firstCollection(){
-        $numbers = [1, 9, 3, 4, 5, 3, 5, 7];
-        $collection = new Collection($numbers);
-        dump($collection);
+        $collections = new Collection($this->numbers);
+        dump($collections);
     }
 
     public function secondCollection(){
-
+        $collections = new Collection(['study', 'laravel', 'uncover'] + $this->numbers);
+        echo $collections[0].'<br>';
+        echo $collections[1].'<br>';
+        foreach($collections as $collection){
+            echo "$collection ";
+        }
+        echo '<br>';
+        $collections = collect([
+            "nama" => "Laura",
+            "sekolah" => "SMA 5 Lampung",
+            "kota" => "Lampung",
+            "jurusan" => "IPA",
+        ]);
+        echo $collections.'<br>';
+        $varA = collect([1,2,3]);
+        $varB = collect([1 => 1, 2 => 2, 3 => 3]);
+        echo $varA.'<br>';
+        echo $varB.'<br>';
     }
 
     public function thirdCollection(){
