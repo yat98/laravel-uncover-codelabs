eb5e3317b2c10d9a3b2a0e65942ae7942ce72084
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-06-10 17:02:47 +0800

    16.3 Pengaksesan model

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function checkObject(){
+        $student = new Student;
+        dump($student);
+    }
 }
