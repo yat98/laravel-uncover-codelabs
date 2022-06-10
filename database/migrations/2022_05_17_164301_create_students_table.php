c9291a7e35fd92d0e8c0f7be1c76bc4a038204ec
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-06-10 16:01:54 +0800

    16.2 Pembuatan model

---
+++ database/migrations/2022_05_17_164301_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim', 8)->unique();
+            $table->string('name');
+            $table->date('birthdate');
+            $table->decimal('gpa',3,2)->default(1.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
