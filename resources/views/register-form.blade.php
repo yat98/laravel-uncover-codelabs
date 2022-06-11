2242a9518ba0a01d92c4ea54a7da2c3bae15eaa7
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-06-12 01:09:22 +0800

    17.7 Repopulate form

--- resources/views/register-form.blade.php
+++ resources/views/register-form.blade.php
@@ -17,21 +17,21 @@
                     @csrf
                     <div class="form-group">
                         <label for="nim">NIM</label>
-                        <input type="text" class="form-control @error('nim') is-invalid @enderror" id="nim" name="nim">
+                        <input type="text" class="form-control @error('nim') is-invalid @enderror" id="nim" name="nim" value="{{ old('nim') }}">
                         @error('nim')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group">
                         <label for="name">Full Name</label>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name">
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
                         @error('name')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email">
+                        <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
                         @error('email')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -41,12 +41,12 @@
                         <div class="form-group">
                             <div class="form-check form-check-inline @error('gender') is-invalid @enderror my-0">
                                 <input class="form-check-input" type="radio" name="gender"
-                                id="male" value="M">
+                                id="male" value="M" @if(old('gender') == 'M') checked @endif>
                                 <label class="form-check-label" for="male">Male</label>
                             </div>
                             <div class="form-check form-check-inline my-0">
                                 <input class="form-check-input" type="radio" name="gender"
-                                id="female" value="F">
+                                id="female" value="F" @if(old('gender') == 'F') checked @endif>
                                 <label class="form-check-label" for="female">Female</label>
                             </div>
                             @error('gender')
@@ -57,11 +57,11 @@
                     <div class="form-group">
                         <label for="major">Major</label>
                         <select class="form-control @error('major') is-invalid @enderror" name="major" id="major">
-                            <option value="Teknik Informatika">Teknik Informatika</option>
-                            <option value="Sistem Informasi">Sistem Informasi</option>
-                            <option value="Ilmu Komputer">Ilmu Komputer</option>
-                            <option value="Teknik Komputer">Teknik Komputer</option>
-                            <option value="Teknik Telekomunikasi">Teknik Telekomunikasi</option>
+                            <option value="Teknik Informatika" {{ old('major') == 'Teknik Informatika' ? 'selected':'' }}>Teknik Informatika</option>
+                            <option value="Sistem Informasi" {{ old('major') == 'Sistem Informasi' ? 'selected':'' }}>Sistem Informasi</option>
+                            <option value="Ilmu Komputer" {{ old('major') == 'Ilmu Komputer' ? 'selected':'' }}>Ilmu Komputer</option>
+                            <option value="Teknik Komputer" {{ old('major') == 'Teknik Komputer' ? 'selected':'' }}>Teknik Komputer</option>
+                            <option value="Teknik Telekomunikasi" {{ old('major') == 'Teknik Telekomunikasi' ? 'selected':'' }}>Teknik Telekomunikasi</option>
                         </select>
                         @error('major')
                             <div class="invalid-feedback">{{ $message }}</div>
@@ -70,7 +70,7 @@
                     <div class="form-group">
                         <label for="address @error('address') is-invalid @enderror">Address</label>
                         <textarea class="form-control" id="address" rows="3"
-                        name="address"></textarea>
+                        name="address">{{ old('nim') }}</textarea>
                         @error('address')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
