141431fee430c0950e5469d469e940ad4942e3f4
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-06-20 22:04:52 +0800

    20.2 Informasi file upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -11,6 +11,15 @@ class FileUploadController extends Controller
     }
 
     public function fileUploadProcess(Request $request){
-        return 'upload file process here';
+        if($request->has('file')){
+            echo 'path(): '.$request->file->path().'<br>';
+            echo 'extension(): '.$request->file->extension().'<br>';
+            echo 'getClientOriginalExtension(): '.$request->file->getClientOriginalExtension().'<br>';
+            echo 'getMimeType(): '.$request->file->getMimeType().'<br>';
+            echo 'getClientOriginalName(): '.$request->file->getClientOriginalName().'<br>';
+            echo 'getSize(): '.$request->file->getSize().'<br>';
+        }else{
+            echo 'Empty file upload';
+        }
     }
 }
