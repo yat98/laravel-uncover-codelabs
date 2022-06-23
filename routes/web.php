f71b2c071619fb0540d31740994972f961c3bb56
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-06-23 11:54:49 +0800

    23.1 Pembuatan route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login', [StudentController::class, 'login']);
+Route::post('login', [StudentController::class, 'getLogin']);
+
+Route::get('logout', [StudentController::class, 'logout']);
+Route::redirect('/', [StudentController::class, 'login']);
+
+Route::middleware('login')->group(function () {
+    Route::get('register', [StudentController::class, 'register']);
+    Route::get('student', [StudentController::class, 'index']);
+    Route::get('student-blog', [StudentController::class, 'blog']);
 });
