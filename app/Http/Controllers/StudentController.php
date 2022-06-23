e011cb313e942de90ce3e454c5c7e1507bf56df0
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-06-23 12:02:07 +0800

    23.2 Pembuatan controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function login(){
+        return view('form-login');
+    }
+
+    public function getLogin(Request $request){
+        $request->validate([
+            'username' => 'required'
+        ]);
+
+        $validUsername = ['john', 'james', 'albert', 'brad'];
+        if(in_array($request->username, $validUsername)){
+            session(['username' => $request->username]);
+            return redirect('student');
+        }else{
+            return back()->withInput()->with('message', 'Username not valid');
+        }
+    }
+
+    public function logout(){
+        session()->forget('username');
+        return redirect('login')->with('message', 'Logout success');
+    }
+
+    public function register(){
+        return view('page',['title' => 'Register Student']);
+    }
+
+    public function index(){
+        return view('page',['title' => 'All Student']);
+    }
+
+    public function blog(){
+        return view('page',['title' => 'Blog Student']);
+    }
 }
