5c78e9ad531ee4eec9c262e3f5e220701394ec2d
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-07-03 20:23:55 +0800

    24.5 Menampilkan data user

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StudentController extends Controller
 {
@@ -10,8 +11,22 @@ class StudentController extends Controller
         return view('page',['title' => 'List Student']);
     }
 
-    public function all(){
-        return view('page',['title' => 'Table Student']);
+    public function all(Request $request){
+        // echo Auth::user()->id.'<br>';
+        // echo Auth::user()->name.'<br>';
+        // echo Auth::user()->email.'<br>';
+        // echo Auth::user()->password.'<br>';
+
+        // echo $request->user()->id.'<br>';
+        // echo $request->user()->name.'<br>';
+        // echo $request->user()->email.'<br>';
+        // echo $request->user()->password.'<br>';
+        // dump(Auth::user());
+        if(Auth::check()){
+            return view('page',['title' => 'Welcome to Table Student, '.$request->user()->name]);
+        }else{
+            return redirect('login');
+        }
     }
 
     public function blog(){
