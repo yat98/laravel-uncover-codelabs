73fe29468134d00384ff0876009657c14c2b24c8
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-07-23 14:42:17 +0800

    27.17 Modifikasi route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('users/{user}/edit',[UserController::class,'edit']);
+Route::get('users/{user}',[UserController::class,'update']);
+Route::get('users/{user}',[UserController::class,'destroy']);
