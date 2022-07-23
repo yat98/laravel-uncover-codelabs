45e48e97533c6c79aaedbcdada7701a68a7a2b94
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-07-23 14:50:25 +0800

    27.19 Menyiapkan data update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function edit(User $user)
+    {
+        $time = strtotime($user->birthdate);
+
+        $user['day'] = date('d',$time);
+        $user['month'] = date('m',$time);
+        $user['year'] = date('y',$time);
+
+        return view('user.edit', compact('user'));
+    }
+
 }
